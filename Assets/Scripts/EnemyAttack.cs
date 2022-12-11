cfdc77abf19fa9658742ad4a70ebdc88579c92f3
rodrobry <rodrobry@gmail.com>
2022-12-11 16:00:48 +0000

    Add scene management

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -7,23 +7,26 @@ public class EnemyAttack : MonoBehaviour
     public float missileSpeed = 7f;
     public float attackSpeed = 2f;
     public GameObject plateObject;
+    public GameObject player;
     private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        player = GameObject.FindWithTag("Player");
         InvokeRepeating("Attack", 2f, attackSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     private void Attack()
     {
+        if(player == null) return;
         animator.Play("MageAttack");
         Invoke("Fire", 0.1f);
         Invoke("ResetAnim", 0.3f);
@@ -38,7 +41,7 @@ public class EnemyAttack : MonoBehaviour
     {
         //Give rotation matching player position
         Vector2 enemyPos = transform.position;
-        Vector2 playerPos = GameObject.FindWithTag("Player").transform.position;
+        Vector2 playerPos = player.transform.position;
         Vector2 targetDirection = (playerPos - enemyPos).normalized;
         float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.Euler(0, 0, angle);
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    private Scene scene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Start game when space is pressed
+        if (Input.GetButtonDown("Jump") && scene.buildIndex == 0)
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        //Start game when space is pressed
+        if (Input.GetButtonDown("Jump") && scene.buildIndex == 1)
+        {
+            SceneManager.LoadScene(2);
+        }
+
+        //Return to main menu when esc is pressed
+        if (Input.GetButtonDown("Cancel") && scene.buildIndex != 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        //Jump to game over scene when player dies
+        var player = GameObject.FindWithTag("Player");
+        if (player == null  && scene.buildIndex == 2)
+        {
+            SceneManager.LoadScene(3);
+        }
+
+        //Restart game from game over scene if space is pressed
+        if (Input.GetButtonDown("Jump")  && scene.buildIndex == 3)
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+
+
+}
